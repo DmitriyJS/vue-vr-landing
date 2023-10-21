f40778f3441911cab16eaac71b06be6670f26347
DmitriyJS <forudemymail888@gmail.com>
2023-10-21 13:51:13 +0300

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'vue-vr',
+  base: process.env.NODE_ENV === 'production' ? '/vue-vr-landing/' : '/',
   plugins: [
     vue(),
   ],
